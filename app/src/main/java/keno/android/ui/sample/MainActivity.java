510d57d0b35c15ed8c735f3207f22e2a62cb4678
阚健雄 <kanjianxiong@rlair.net>
2019-11-20 15:28:06 +0800

    ConstraintLayout Sample

--- app/src/main/java/keno/android/ui/sample/MainActivity.java
+++ app/src/main/java/keno/android/ui/sample/MainActivity.java
@@ -1,13 +1,39 @@
 package keno.android.ui.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnRelativePosition;
+    private Button btnMarginGone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        btnRelativePosition = findViewById(R.id.btn_relative_position);
+        btnMarginGone = findViewById(R.id.btn_margin_gone);
+
+        btnRelativePosition.setOnClickListener(this);
+        btnMarginGone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_relative_position:
+                RelaytivePositionActivity.start(this);
+                break;
+            case R.id.btn_margin_gone:
+
+                break;
+        }
     }
 }
---
+++ app/src/main/java/keno/android/ui/sample/RelaytivePositionActivity.java
@@ -0,0 +1,20 @@
+package keno.android.ui.sample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelaytivePositionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relaytive_position);
+    }
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, RelaytivePositionActivity.class);
+        context.startActivity(starter);
+    }
+}
