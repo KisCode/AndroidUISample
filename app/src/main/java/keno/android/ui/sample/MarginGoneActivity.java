cc7375b1c71368489d9f4ebe9a2941956b5c22ba
kisCode <keno123@126.com>
2019-11-20 16:37:37 +0800

    Margins when connected to a GONE widget

---
+++ app/src/main/java/keno/android/ui/sample/MarginGoneActivity.java
@@ -0,0 +1,13 @@
+package keno.android.ui.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarginGoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_margin_gone);
+    }
+}
