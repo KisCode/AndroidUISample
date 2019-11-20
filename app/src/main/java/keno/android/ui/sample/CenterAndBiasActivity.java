25ac1d0a2be97ed78697462be2f50f6762ba605c
kisCode <keno123@126.com>
2019-11-20 16:54:58 +0800

    layout_goneMarginStart demo

---
+++ app/src/main/java/keno/android/ui/sample/CenterAndBiasActivity.java
@@ -0,0 +1,13 @@
+package keno.android.ui.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CenterAndBiasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_center_and_bias);
+    }
+}
--- app/src/main/java/keno/android/ui/sample/MarginGoneActivity.java
+++ app/src/main/java/keno/android/ui/sample/MarginGoneActivity.java
@@ -9,6 +9,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+/**
+ *
+设置 GONE属性后，控件就不会出现在布局中了，B控件对A控件的margin属性也就没有作用了。
+ 但是 ConstraintLayout 能对已经设置 GONE属性的控件进行特殊处理。当A控件设置 GONE之后，A控件相当于变成了一个点，B控件相对于对A的约束仍然是起作用的。
+ */
 public class MarginGoneActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView ivAvatar;
@@ -44,12 +49,14 @@ public class MarginGoneActivity extends AppCompatActivity implements View.OnClic
         switch (v.getId()) {
             case R.id.btn_gone:
                 tvUsername.setVisibility(View.GONE);
+                //btnVisible 距离左边控件btnGone marginStart = 20dp，layout_goneMarginStart=0 ,当btnGone设置为gone时，layout_goneMarginStart 生效
+                btnGone.setVisibility(View.GONE);
                 break;
             case R.id.btn_visible:
                 tvUsername.setVisibility(View.VISIBLE);
+
+                btnGone.setVisibility(View.VISIBLE);
                 break;
         }
     }
-//    设置 GONE属性后，控件就不会出现在布局中了，B控件对A控件的margin属性也就没有作用了。
-//但是 ConstraintLayout 能对已经设置 GONE属性的控件进行特殊处理。当A控件设置 GONE之后，A控件相当于变成了一个点，B控件相对于对A的约束仍然是起作用的。
 }
