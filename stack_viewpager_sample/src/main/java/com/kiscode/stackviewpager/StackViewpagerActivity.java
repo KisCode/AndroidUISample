c7706ad922957e9f8b41307b7da6b4902abe4302
kisCode <keno123@126.com>
2021-02-24 15:36:06 +0800

    suppor Viewpager notifyDataSetChange update UI

--- stack_viewpager_sample/src/main/java/com/kiscode/stackviewpager/StackViewpagerActivity.java
+++ stack_viewpager_sample/src/main/java/com/kiscode/stackviewpager/StackViewpagerActivity.java
@@ -12,7 +12,10 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.kiscode.stackviewpager.adapter.CardFragmentAdapter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Description:
@@ -21,10 +24,12 @@ import java.util.Arrays;
  **/
 public class StackViewpagerActivity extends AppCompatActivity {
     private static final String TAG = "StackViewpager";
-
     private static final int SIZE_OFFSET = 40;
-    private ViewPager viewPager;
+    final String[] contentArr = {"One", "Two", "Three", "Four", "Five", "Six"};
+    private boolean mIsRandom = true;
     private float actionDownX, actionDownY;
+    private ViewPager viewPager;
+    private CardFragmentAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +44,10 @@ public class StackViewpagerActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        String[] contentArr = {"One", "Two", "Three", "Four", "Five", "Six"};
         viewPager = findViewById(R.id.viewpager_stack);
         viewPager.setOffscreenPageLimit(5);
-        viewPager.setAdapter(new CardFragmentAdapter(getSupportFragmentManager(), Arrays.asList(contentArr)));
+        adapter = new CardFragmentAdapter(getSupportFragmentManager(), Arrays.asList(contentArr));
+        viewPager.setAdapter(adapter);
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -72,12 +77,17 @@ public class StackViewpagerActivity extends AppCompatActivity {
                 } else {
                     //设置每个View在中间，即设置相对原位置偏移量
                     page.setTranslationX((-page.getWidth() * position));
-                    //缩放比例
-                    float scale = (page.getWidth() - SIZE_OFFSET * position) / (float) page.getWidth();
-                    page.setScaleX(scale);
-                    page.setScaleY(scale);
-
                     page.setTranslationY(SIZE_OFFSET * position);
+
+                    if (page.getWidth() > 0) {
+                        //缩放比例
+                        float scale = (page.getWidth() - SIZE_OFFSET * position) / (float) page.getWidth();
+
+                        Log.i("transformPageScale", "scale:" + scale + "\tposition:" + position + "\tpage.getWidth()\t" + page.getWidth());
+                        page.setScaleX(scale);
+                        page.setScaleY(scale);
+                    }
+
                 }
 
                 Log.i("transformPage", "position:" + position);
@@ -85,7 +95,7 @@ public class StackViewpagerActivity extends AppCompatActivity {
         });
 
         viewPager.setOnTouchListener((v, event) -> {
-            if (viewPager.getCurrentItem() < contentArr.length - 1) {
+            if (viewPager.getCurrentItem() < adapter.getCount() - 1) {
                 return false;
             }
 
@@ -95,13 +105,13 @@ public class StackViewpagerActivity extends AppCompatActivity {
             } else if (event.getAction() == MotionEvent.ACTION_UP) {
                 if (actionDownX - event.getX() > 50) { //向左滑
                     Log.i(TAG, actionDownX + "\t" + actionDownY + "\tevent:\tx = " + event.getX() + ", y = " + event.getY());
-                    Toast.makeText(StackViewpagerActivity.this, "Detect left scroll...", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(StackViewpagerActivity.this, "Detect left scroll...", Toast.LENGTH_SHORT).show();
+                    reloadData();
                     return true;
                 }
             }
             return false;
         });
-
 
         viewPager.setOnGenericMotionListener(new View.OnGenericMotionListener() {
             @Override
@@ -109,5 +119,22 @@ public class StackViewpagerActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void reloadData() {
+        List<String> list = new ArrayList<>();
+        if (mIsRandom) {
+            int startNum = new Random().nextInt(90);
+            for (int i = startNum; i < startNum + 6; i++) {
+                list.add(String.valueOf(i));
+            }
+            mIsRandom = false;
+        } else {
+            list = Arrays.asList(contentArr);
+            mIsRandom = true;
+        }
+
+        adapter.setNewDatas(list);
+        viewPager.setCurrentItem(0);
     }
 }
