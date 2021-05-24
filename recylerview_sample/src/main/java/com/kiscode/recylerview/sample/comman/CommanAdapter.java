c70b10498532867837794afbca5fb07d81247a4e
kisCode <keno123@126.com>
2021-05-24 17:06:07 +0800

    优化了通用适配器点击事件 移动到onCreateViewHolder去除冗余setOnItemClick事件

--- recylerview_sample/src/main/java/com/kiscode/recylerview/sample/comman/CommanAdapter.java
+++ recylerview_sample/src/main/java/com/kiscode/recylerview/sample/comman/CommanAdapter.java
@@ -2,7 +2,6 @@ package com.kiscode.recylerview.sample.comman;
 
 
 import android.content.Context;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.LayoutRes;
@@ -11,15 +10,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-/****
- * ProjectName: AndroidUISample
- * Package: com.kiscode.recylerview.sample.comman
- * ClassName: CommanAdapter
+/**
+ * 通用内容适配器
  * Description:
- * Author:  Administrator
- * CreateDate: 2020/2/16 11:05
- */
-
+ * Author: keno
+ * Date : 2020/2/16 11:05
+ **/
 public abstract class CommanAdapter<T> extends RecyclerView.Adapter<CommanViewHolder> {
     protected List<T> mDatas;
     protected Context mContext;
@@ -27,6 +23,7 @@ public abstract class CommanAdapter<T> extends RecyclerView.Adapter<CommanViewHo
     protected @LayoutRes
     int mLayoutRes;
     private OnItemClickListener<T> onItemClickListener;
+    private OnItemLongClickListener<T> onItemLongClickListener;
 
     public CommanAdapter(List<T> mDatas, int mLayoutRes) {
         this.mDatas = mDatas;
@@ -37,25 +34,32 @@ public abstract class CommanAdapter<T> extends RecyclerView.Adapter<CommanViewHo
     @Override
     public CommanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         mContext = parent.getContext();
-        return CommanViewHolder.get(mContext, parent, mLayoutRes);
+        CommanViewHolder commanViewHolder = CommanViewHolder.get(mContext, parent, mLayoutRes);
+        bindViewClickListener(commanViewHolder);
+        return commanViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull CommanViewHolder holder, final int position) {
         convert(holder, position);
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (onItemClickListener != null) {
-                    onItemClickListener.onClick(CommanAdapter.this, position);
-                }
-            }
-        });
     }
 
     @Override
     public int getItemCount() {
         return mDatas.size();
+    }
+
+    private void bindViewClickListener(final CommanViewHolder commanViewHolder) {
+        if (onItemClickListener != null) {
+            commanViewHolder.itemView.setOnClickListener(v -> onItemClickListener.onClick(CommanAdapter.this, commanViewHolder.getLayoutPosition()));
+        }
+
+        if (onItemLongClickListener != null) {
+            commanViewHolder.itemView.setOnLongClickListener(v -> {
+                onItemLongClickListener.onLongClick(CommanAdapter.this, commanViewHolder.getLayoutPosition());
+                return true;
+            });
+        }
     }
 
     public T getItem(int pos) {
@@ -75,7 +79,15 @@ public abstract class CommanAdapter<T> extends RecyclerView.Adapter<CommanViewHo
         this.onItemClickListener = onItemClickListener;
     }
 
+    public void setOnItemLongClickListener(OnItemLongClickListener<T> onItemLongClickListener) {
+        this.onItemLongClickListener = onItemLongClickListener;
+    }
+
     public interface OnItemClickListener<T> {
         void onClick(CommanAdapter<T> adapter, int pos);
+    }
+
+    public interface OnItemLongClickListener<T> {
+        void onLongClick(CommanAdapter<T> adapter, int pos);
     }
 }
